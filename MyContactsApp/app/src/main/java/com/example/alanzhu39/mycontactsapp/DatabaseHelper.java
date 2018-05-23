fda60cb14b34a2ea146dde605e3144533d680210
Alan Zhu <alan.zhu39@gmail.com>
2018-05-22 20:30:57 -0700

    DatabaseHelper

---
+++ MyContactsApp/app/src/main/java/com/example/alanzhu39/mycontactsapp/DatabaseHelper.java
@@ -0,0 +1,33 @@
+package com.example.alanzhu39.mycontactsapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Contact.db";
+    public static final String TABLE_NAME = "Contact_table";
+    public static final String ID = "ID";
+    public static final String COLUMN_NAME_CONTACT = "contact";
+
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NAME_CONTACT + " TEXT";
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
