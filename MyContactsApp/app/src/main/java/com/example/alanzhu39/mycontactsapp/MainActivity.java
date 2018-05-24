82eaf2cabe75a5cf39db1aa2de3106dee231d7c4
Alan Zhu <alan.zhu39@gmail.com>
2018-05-24 09:00:10 -0700

    Address and Phone fields

--- MyContactsApp/app/src/main/java/com/example/alanzhu39/mycontactsapp/MainActivity.java
+++ MyContactsApp/app/src/main/java/com/example/alanzhu39/mycontactsapp/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.alanzhu39.mycontactsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDB;
+    EditText editName;
+    EditText editPhone;
+    EditText editAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +20,24 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MyContactApp","MainActivity: setting up layout");
         setContentView(R.layout.activity_main);
 
+        editName = findViewById(R.id.editText_name);
+        editPhone = findViewById(R.id.editText_phone);
+        editAddress = findViewById(R.id.editText_address);
+
         myDB = new DatabaseHelper(this);
         Log.d("MyContactApp","MainActivity: instantiated DatabaseHelper");
     }
+
+    public void addData(View view) {
+        Log.d("MyContactApp","MainActivity: add contact button pressed");
+        boolean isInserted = myDB.insertData(editName.getText().toString(),editPhone.getText().toString(),editAddress.getText().toString());
+        if(isInserted) {
+            Toast.makeText(MainActivity.this,"Success - contact inserted", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this,"Failed - contact not inserted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
